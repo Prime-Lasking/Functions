244b8cab6fffafbe357e1a6a749c338871429883
The Prime Mathematician <197593373+Prime-Lasking@users.noreply.github.com>
2025-04-09 21:02:23 +1000

    Update sigma function.py

--- sigma function.py
+++ sigma function.py
@@ -1,7 +1,8 @@
+N = int(input('what number do you want to put in? '))
 def sigma(n):
     x=0
     for i in range(1, n+1):
         if n%i==0:
             x+=i
     return x
-print(sigma(int(input('what number do you want to put in? '))))
+print(sigma(N))
